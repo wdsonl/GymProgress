22c401b99d7bf6b2a8d9d99064355a0ad9f3e869
wdsonl <wdsonlucasdefreitas@gmail.com>
2022-11-01 14:59:58 -0300

    teste

--- app/src/main/java/com/example/gymprogress/MainActivity.java
+++ app/src/main/java/com/example/gymprogress/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
     private ImageButton peitoBt;
-
+    //kkkkk1
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
