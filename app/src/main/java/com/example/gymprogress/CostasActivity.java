09c0a8e8e286c1b716aa14fceaee250c251428f9
Miguelav1029 <miguelsorgpinheiro.msp@gmail.com>
2022-11-12 15:07:09 -0300

    -a activity cosstas tem uma listview q mostra todos os exercicioss -falta adicionar os exercicios -falta filtrar na lista quais exercicios sao "costas" pra mostrar apenas essses

--- app/src/main/java/com/example/gymprogress/CostasActivity.java
+++ app/src/main/java/com/example/gymprogress/CostasActivity.java
@@ -5,12 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class CostasActivity extends AppCompatActivity {
 
     private Button voltarBt;
+
+    //listview para exibir a lista de contatos
+    private ListView mylist;
+
+    //adapter da listView
+    private ArrayAdapter adapter;
+
+    //array para a lista de contatos
+    private static ArrayList<exercise> showList;
+
+    private exerciseDB db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +40,16 @@ public class CostasActivity extends AppCompatActivity {
                 startActivity(voltar);
             }
         });
+
+        db = new exerciseDB(this);
+
+        mylist = (ListView) findViewById(R.id.listViewXML);
+        //chama o método findAll que devolve um array e guarda em exibeLista
+        showList = db.showallexercises();
+        //criação de uma instância de um ListAdapter utilizando um layout nativo
+        adapter = new ArrayAdapter<exercise>(this, android.R.layout.simple_list_item_1, showList);
+
+        //associação a ListView com o adapter
+        mylist.setAdapter(adapter);
     }
 }
